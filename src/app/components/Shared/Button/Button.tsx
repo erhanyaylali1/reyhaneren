50dc2205cbdfccc40a50b06cd128be2d6e8a6558
erhan <erhanyaylali9@gmail.com>
2024-11-16 00:33:54 +0100

    Typohraphy and Button components are created

---
+++ src/app/components/Shared/Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./button.module.scss";
+
+export type IButtonProps = {
+    title: string;
+    ariaLabel: string;
+    variant?: ButtonVariants;
+    size?: ButtonSizes;
+    disabled?: boolean;
+    handleClick: () => void;
+}
+
+export enum ButtonSizes {
+    "sm" = "sm",
+    "md" = "md",
+    "lg" = "lg",
+}
+
+export enum ButtonVariants {
+    "primary" = "primary",
+    "secondary" = "secondary",
+    "yellow" = "yellow",
+}
+
+const ButtonComponent = ({ title, ariaLabel, handleClick, variant = ButtonVariants.secondary, size = ButtonSizes.md, disabled = false }: IButtonProps): React.ReactNode => {
+    return (
+        <button
+            className={`${styles.button} ${styles["button__" + size]} ${styles["button__" + variant]}`}
+            disabled={disabled}
+            aria-label={ariaLabel}
+            onClick={handleClick}
+        >
+            {title}
+        </button>
+    )
+}
+
+export default ButtonComponent;
---
+++ src/app/components/Shared/Typography/Typography.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import styles from "./typography.module.scss";
+import { Noto_Sans, Saira } from 'next/font/google'
+
+
+type ITypography = {
+    text: string;
+    type?: TextType;
+    as?: TextAsTypes;
+    color?: TextColors;
+    fontWeight?: TextFontWeights;
+    fontSizeDesktop?: TextFontSizes;
+    fontSizeMobile?: TextFontSizes;
+}
+
+export enum TextColors {
+    "light" = "light",
+    "black" = "black",
+    "dark" = "dark",
+    "yellow" = "yellow",
+    "secondary" = "secondary"
+}
+
+export enum TextFontWeights {
+    "light" = "light",
+    "regular" = "regular",
+    "medium" = "medium",
+    "semibold" = "semibold",
+    "bold" = "bold",
+}
+
+export enum TextFontSizes {
+    "12px" = "12px",
+    "14px" = "14px",
+    "16px" = "16px",
+    "18px" = "18px",
+    "20px" = "20px",
+    "22px" = "22px",
+    "24px" = "24px",
+    "28px" = "28px",
+    "30px" = "30px",
+    "32px" = "32px",
+    "48px" = "48px",
+    "64px" = "64px"
+}
+
+export enum TextType {
+    "body" = "body",
+    "title" = "title"
+}
+
+export enum TextAsTypes {
+    "h1" = "h1",
+    "h2" = "h2",
+    "h3" = "h3",
+    "h4" = "h4",
+    "h5" = "h5",
+    "h6" = "h6",
+    "p" = "p",
+    "span" = "span",
+}
+
+const noto_sans = Noto_Sans({
+    weight: ['100', '200', '300', '400', '500', '600', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+})
+
+const saira = Saira({
+    weight: ['100', '200', '300', '400', '500', '600', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+})
+
+const Typography = ({ text, type = TextType.body, as = TextAsTypes.p, color = TextColors.dark, fontWeight = TextFontWeights.regular, fontSizeDesktop = TextFontSizes["18px"], fontSizeMobile = TextFontSizes["16px"] }: ITypography): React.ReactNode => {
+
+    const TextComponent = as;
+
+    return (
+        <TextComponent className={`${styles["text-" + color]} ${styles["text-" + fontWeight]} ${type === TextType.body ? noto_sans.className : saira.className}`}>
+            {text}
+        </TextComponent>
+    )
+}
+
+export default Typography;
